2c43c6365fdf84bdb03bf39be798b335cd3496c8
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-06 20:39:58 -0300

    cap10-overviewReactComponents

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,2 +1,12 @@
 import React from 'react'
 import { Text, StyleSheet } from 'react-native'
+
+const ComponentsScreen =()=>{
+    return <text>THis is the components screen</text>
+}
+
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize: 30s
+    }
+})

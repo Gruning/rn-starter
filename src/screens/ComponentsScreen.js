2a0368c29b8c529c49a2d96e0a33d9694badba12
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-18 19:28:44 -0300

    cap13-rulesJsx

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 
 const ComponentsScreen =()=>{
+    const medio= 'youtube'
     return (
         <View>
             <Text style={styles.textStyle}>Precalculo</Text>
             <Text>Larson 2018</Text>
             <Text>Stewart 7ma edicion</Text>
+            <Text style={styles.textStyle}>Estadistica</Text>
+            <Text>Introduccion  la estadistica</Text>
+            <Text style={styles.textStyle}>Curso CISCO</Text>
+            <Text>buscar youtube</Text>
+            <Text style={styles.textStyle}>Curso C++</Text>
+            <Text>buscar en  {medio} tutoriales POO C++</Text>
         </View>
 
     )

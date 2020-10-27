fb7ae2a3b1bcb584ca3a83e6277886c226f31412
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-27 20:40:56 -0300

    cap1-renderFlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,1 +1,16 @@
 import React from 'react'
+import {View, Text,StyleSheet, FlatList} from 'react-native'
+
+const ListScreen = ()=> {
+    const friends =[
+        {name:'chandler'},
+        {name:'monica'},
+        {name:'joey'},
+        {name:'rachel'},
+
+    ]
+     return <Text>List Screen</Text>
+}
+const styles = StyleSheet.create({})
+
+export default ListScreen

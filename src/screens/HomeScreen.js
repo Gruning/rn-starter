9b03e5dda6be2c5fbc499c8d6687300ce8ffdb2a
kimono-server <alberto.gruning.zen@gmail.com>
2020-11-30 23:37:37 +0000

    cap27-touchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet,View, Button } from "react-native";
+import { Text, StyleSheet,View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return <View>
@@ -8,6 +8,10 @@ const HomeScreen = () => {
         onPress={()=>console.log('button pressed')}
         title="Go to Componnents Demo"
       />
+      <TouchableOpacity onPress={()=>{console.log('list pressed')}}>
+        <Text>Go to list Demo</Text>
+        <Text>with touchable opacity</Text>
+      </TouchableOpacity>
   </View> 
 };
 
